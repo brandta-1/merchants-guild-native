2d44009f6c9b600aad1c4cbaf41664a62b548a10
brandta-1 <116298512+brandta-1@users.noreply.github.com>
2024-01-06 20:57:14 -0500

    package.json

--- server/server.js
+++ server/server.js
@@ -18,9 +18,9 @@ app.use(
 );
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../merch-guild-native/web-build')));
     app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+        res.sendFile(path.join(__dirname, '../merch-guild-native/web-build/index.html'));
     });
 }
 
